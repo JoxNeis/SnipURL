3ab141b9a9a3c1ea1e7ae0fdb7e43e2764c377e9
Jox <85673116+JoxNeis@users.noreply.github.com>
2026-05-24 11:36:50 +0700

    Feat: Update root endpoint to display ASCII art

--- SnipURL/Program.cs
+++ SnipURL/Program.cs
@@ -10,7 +10,18 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => 
+"""
+<code>
+  ____        _       _    _ ____  _     
+ / ___| _ __ (_)_ __ | |  | |  _ \| |    
+ \___ \| '_ \| | '_ \| |  | | |_) | |    
+  ___) | | | | | |_) | |__| |  _ <| |___ 
+ |____/|_| |_|_| .__/ \____/|_| \_\_____|
+               |_|   
+</code>
+"""
+  );
 app.MapControllers();
 
 app.Run();
